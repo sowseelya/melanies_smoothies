a609506ae0fe4296e9c368aa16d7b43c4ee26b88
sowseelya <160940842+sowseelya@users.noreply.github.com>
2024-03-12 19:39:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,10 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 
 my_dataframe = session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 ingrediant_list= st.multiselect('choose upto 5 ingredients:', my_dataframe,max_selections=5)
 if ingrediant_list:
